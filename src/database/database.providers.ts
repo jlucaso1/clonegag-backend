f784773b82eeedc8f311feed3063362ecbf872dc
João Lucas de Oliveira Lopes <jlucaso@hotmail.com>
2021-10-13 01:11:56 -0300

    using postgresql instead sqlite

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -5,8 +5,12 @@ export const databaseProviders = [
     provide: 'DATABASE_CONNECTION',
     useFactory: async () =>
       await createConnection({
-        type: 'sqlite',
-        database: 'db.sqlite',
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: parseInt(process.env.DATABASE_HOST),
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: true,
       }),
