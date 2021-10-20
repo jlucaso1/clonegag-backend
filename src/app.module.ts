b0fcbc32fab0a194bd7a5ce6b58ecf98dd75bd92
João Lucas de Oliveira Lopes <jlucaso@hotmail.com>
2021-10-20 15:03:00 -0300

    Added error handling for inputs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { GraphQLError, GraphQLFormattedError } from 'graphql';
 import { AuthModule } from './auth/auth.module';
 import { PostModule } from './post/post.module';
 import { ProfileModule } from './profile/profile.module';
@@ -10,10 +11,43 @@ import { UserModule } from './user/user.module';
   imports: [
     ConfigModule.forRoot(),
     GraphQLModule.forRoot({
-      debug: true,
+      debug: false,
       playground: true,
       autoSchemaFile: 'schema.gql',
       context: ({ req }) => ({ req }),
+      formatError: (error: GraphQLError) => {
+        if (error.message === 'VALIDATION_ERROR') {
+          const extensions = {
+            code: 'VALIDATION_ERROR',
+            errors: [],
+          };
+
+          Object.keys(error.extensions.invalidArgs).forEach((key) => {
+            const constraints = [];
+            Object.keys(error.extensions.invalidArgs[key].constraints).forEach(
+              (_key) => {
+                constraints.push(
+                  error.extensions.invalidArgs[key].constraints[_key],
+                );
+              },
+            );
+
+            extensions.errors.push({
+              field: error.extensions.invalidArgs[key].property,
+              errors: constraints,
+            });
+          });
+
+          const graphQLFormattedError: GraphQLFormattedError = {
+            message: extensions.errors[0].errors[0],
+            extensions: extensions,
+          };
+
+          return graphQLFormattedError;
+        } else {
+          return error;
+        }
+      },
     }),
     UserModule,
     ProfileModule,
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
-import { ValidationPipe } from '@nestjs/common';
+import { UserInputError } from '.pnpm/apollo-server-errors@3.2.0_graphql@15.6.1/node_modules/apollo-server-errors';
+import { ValidationError, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: (errors: ValidationError[]) => {
+        return new UserInputError('VALIDATION_ERROR', {
+          invalidArgs: errors,
+        });
+      },
+    }),
+  );
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
